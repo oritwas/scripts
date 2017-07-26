f7ff5687eedfa45dd76387f27906ed5ba338e282
Orit Wasserman <owasserm@redhat.com>
2017-07-26 11:42:37 +0300

    fix create_scope.sh
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- create_cscope.sh
+++ create_cscope.sh
@@ -5,9 +5,9 @@ find  ./src -type f -name "*.cpp" -print  >>  ./cscope.files
 find  ./src -type f -name "*.hpp" -print  >>  ./cscope.files
 find  ./src -type f -name "*.cc" -print  >> ./cscope.files
 find  ./src -type f -name "*.py" -print  >> ./cscope.files
-find  ./qa -type f -name "*.[chsS]" -print > ./cscope.files
+find  ./qa -type f -name "*.[chsS]" -print >> ./cscope.files
 find  ./qa -type f -name "*.py" -print  >> ./cscope.files
-find  ./s3-tests -type f -name "*.[chsS]" -print > ./cscope.files
+find  ./s3-tests -type f -name "*.[chsS]" -print >> ./cscope.files
 find  ./s3-tests -type f -name "*.py" -print  >> ./cscope.files
 
 
